44d3d944f5a4cac896b31d596a654fdb025721ab
unknown <namitha.802432@sxcce.edu.in>
2025-07-17 09:37:03 +0530

    commit

--- src/app/component/ServiceCard.tsx
+++
@@ -1,8 +0,0 @@
-import {MonitorSmartphone} from "lucide-react";
-import Image from "next/image";
-export default function <ServiceCard>props</ServiceCard>
-{
-    return<>
-    <div classname="flex border-2 border-blue-800"
-    
-}
